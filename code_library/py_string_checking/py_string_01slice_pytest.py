20490ce15f8bfb1bcf5f8e13ce2a2bc2264a9a8a
Sergei <32073541+SSN-SPB@users.noreply.github.com>
2022-09-23 11:02:05 +0200

    Example of string slice functionality with pytest checking (#13)
    Co-authored-by: Sergey <sergei_smirnov@epam.com>

---
+++ code_library/py_string_checking/py_string_01slice_pytest.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+import os
+import re
+import pytest
+
+TEST_STRING = '123_test'
+DEFAULT_STEP = -2
+
+
+def test_reversed_string():
+    converted_string = TEST_STRING[::-1]
+    checked_value = converted_string == 'tset_321'
+    print('Reversing string check is: {}'.format(checked_value))
+    assert checked_value
+
+
+def test_reversed_string_with_step_in_variable():
+    converted_string = TEST_STRING[::DEFAULT_STEP]
+    checked_value = converted_string == 'te_2'
+    print('Reversing string check is with variable: {}'.format(checked_value))
+    assert checked_value
+
+
+def test_reversed_string_with_step_in_variable_negative_assert():
+    converted_string = TEST_STRING[::DEFAULT_STEP]
+    checked_value = converted_string == 'te_2'
+    try:
+        assert checked_value
+        print('Reversing string negative assert is: {}'.format(checked_value))
+    except AssertionError as e:
+        print('Reversing string negative assert is: {}'.format(checked_value))
+
+
+def main():
+    test_string = '123foo.bar.two.three.two.two__com'
+    # reversed string
+    print(test_string[::-1])
+    slice_variable = -1
+    print(test_string[::slice_variable])
+    test_reversed_string()
+    test_reversed_string_with_step_in_variable()
+    test_reversed_string_with_step_in_variable_negative_assert()
+
+
+if __name__ == '__main__':
+    main()
---
+++ code_library/py_string_checking/pytest_string_01slice.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+#!/bin/sh
+
+pytest -v -rPf py_string_01slice_pytest.py
