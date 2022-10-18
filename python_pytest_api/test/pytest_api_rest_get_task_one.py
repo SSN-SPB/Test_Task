4b468f51eea1d9526bb287f3e167ea8027dba324
Sergei <32073541+SSN-SPB@users.noreply.github.com>
2022-10-18 16:50:47 +0200

    Adding constant data from fixture (#16)
    Co-authored-by: Sergey <sergei_smirnov@epam.com>

--- python_pytest_api/test/pytest_api_rest_get_task_one.py
+++ python_pytest_api/test/pytest_api_rest_get_task_one.py
@@ -1,11 +1,17 @@
 #!/usr/bin/env python3
 import json
 import requests
+import pytest
 
 
 TESTED_PAIR = 'USDRUB'
 ENV_URL = 'https://www.freeforexapi.com/api/live?pairs='
 ENDPOINT = ENV_URL + TESTED_PAIR
+
+
+@pytest.fixture()
+def expected_code():
+    return {'OK': 200, 'NotKnownPair': 1002}
 
 
 def get_pair_data(endpoint):
@@ -29,10 +35,20 @@ def test_rates_exists():
     assert checked_value
 
 
-def test_code_is_200():
+@pytest.mark.check_response_code
+def test_code_is_200(expected_code):
     tested_response = get_pair_data(ENDPOINT)
-    checked_value = tested_response['code'] == 200
+    checked_value = tested_response['code'] == expected_code['OK']
     print('The code = 200 is: {}'.format(checked_value))
+    assert checked_value
+
+
+@pytest.mark.check_response_code
+def test_code_unknown_pair_request(expected_code):
+    tested_response = get_pair_data(ENV_URL + 'bad_endpoint_string')
+    checked_value = tested_response['code'] == expected_code['NotKnownPair']
+    print('The bad code = {} is: {}'.format(expected_code['NotKnownPair'],
+                                            checked_value))
     assert checked_value
 
 
