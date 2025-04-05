10c8c90de1a1478c9776f69a49f4ffbb6537e0cc
Sergei Smirnov6 <sergei_smirnov6@epam.com>
2025-04-06 00:14:07 +0200

    code style refactoring

--- Py_functionality_libs/py_decorators/py_decorator_with_arguments.py
+++ Py_functionality_libs/py_decorators/py_decorator_with_arguments.py
@@ -4,11 +4,15 @@ def repeat(times):
             for _ in range(times):
                 result = func(*args, **kwargs)
             return result
+
         return wrapper
+
     return decorator
+
 
 @repeat(3)
 def say_hello():
     print("Hello!")
+
 
 say_hello()
