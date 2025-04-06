17015b3f31566d76741288b3ed7570c49d21118e
Sergei Smirnov6 <sergei_smirnov6@epam.com>
2025-04-06 15:12:38 +0200

    Code stile updating

--- Py_functionality_libs/jira_handling/jira_handling_01_BASIC_01_NI.py
+++ Py_functionality_libs/jira_handling/jira_handling_01_BASIC_01_NI.py
@@ -7,10 +7,10 @@ import os
 
 # suppress all warnings
 import warnings
+
 warnings.filterwarnings("ignore")
 
-target_of_file = 'Jira treating'
-
+target_of_file = "Jira treating"
 
 
 def main():
@@ -19,23 +19,23 @@ def main():
     # issue with SSL - SSL: CERTIFICATE_VERIFY_FAILED
     # ingored by 'verify':False
     # Remark: the warning is supressed. See above
-    USER = os.getenv('LOGON_JIRA_NI')
-    PASSWORD = os.getenv('LOGON_JIRA_NI_PASSWORD')
-    print('used logon: {}'.format(USER))
-    print('used password: {}'.format(PASSWORD))
-    jira_optionsNI = {'server': 'https://jira.network.ae/jira' , 'verify':False}
-    jiraNI = JIRA(options = jira_optionsNI, basic_auth=(USER, PASSWORD))
-    if (jiraNI):
-        print('Logon to Jira is successful')
+    USER = os.getenv("LOGON_JIRA_NI")
+    PASSWORD = os.getenv("LOGON_JIRA_NI_PASSWORD")
+    print("used logon: {}".format(USER))
+    print("used password: {}".format(PASSWORD))
+    jira_optionsNI = {"server": "https://jira.network.ae/jira", "verify": False}
+    jiraNI = JIRA(options=jira_optionsNI, basic_auth=(USER, PASSWORD))
+    if jiraNI:
+        print("Logon to Jira is successful")
     else:
-        print('Logon to Jira is successful')
+        print("Logon to Jira is successful")
     query_duedate_past = """key in (MAF-4781, MAF-4785, MAF-4768)"""
-    issue = jiraNI.issue('MAF-4768')
-    print(issue) # repsonse: MAF-4768
-    print(issue.fields.project.key) # 'MAF'
-    print(issue.fields.issuetype.name) # 'User Story'
-    print(issue.fields.reporter.displayName) # 'Pavel Pokrovskii'
+    issue = jiraNI.issue("MAF-4768")
+    print(issue)  # repsonse: MAF-4768
+    print(issue.fields.project.key)  # 'MAF'
+    print(issue.fields.issuetype.name)  # 'User Story'
+    print(issue.fields.reporter.displayName)  # 'Pavel Pokrovskii'
 
 
-
-if __name__ == '__main__': main()
+if __name__ == "__main__":
+    main()
--- Py_functionality_libs/jira_handling/jira_handling_01_BASIC_01_NI_SQL.py
+++ Py_functionality_libs/jira_handling/jira_handling_01_BASIC_01_NI_SQL.py
@@ -8,10 +8,10 @@ import os
 
 # suppress all warnings
 import warnings
+
 warnings.filterwarnings("ignore")
 
-target_of_file = 'Jira treating'
-
+target_of_file = "Jira treating"
 
 
 def main():
@@ -20,24 +20,31 @@ def main():
     # issue with SSL - SSL: CERTIFICATE_VERIFY_FAILED
     # ingored by 'verify':False
     # Remark: the warning is supressed. See above
-    USER = os.getenv('LOGON_JIRA_NI')
-    PASSWORD = os.getenv('LOGON_JIRA_NI_PASSWORD')
-    print('used logon: {}'.format(USER))
-    print('used password: {}'.format(PASSWORD))
-    jira_optionsNI = {'server': 'https://jira.network.ae/jira' , 'verify':False}
-    jiraNI = JIRA(options = jira_optionsNI, basic_auth=(USER, PASSWORD))
-    if (jiraNI):
-        print('Logon to Jira is successful')
+    USER = os.getenv("LOGON_JIRA_NI")
+    PASSWORD = os.getenv("LOGON_JIRA_NI_PASSWORD")
+    print("used logon: {}".format(USER))
+    print("used password: {}".format(PASSWORD))
+    jira_optionsNI = {"server": "https://jira.network.ae/jira", "verify": False}
+    jiraNI = JIRA(options=jira_optionsNI, basic_auth=(USER, PASSWORD))
+    if jiraNI:
+        print("Logon to Jira is successful")
     else:
-        print('Logon to Jira is successful')
+        print("Logon to Jira is successful")
     query_duedate_past = """key in (MAF-4781, MAF-4785, MAF-4768)"""
-    issue = jiraNI.issue('MAF-4768')
-    print(issue) # repsonse: MAF-4768
-    print(issue.fields.project.key) # 'MAF'
-    print(issue.fields.issuetype.name) # 'User Story'
-    print(issue.fields.reporter.displayName) # 'Pavel Pokrovskii'
+    issue = jiraNI.issue("MAF-4768")
+    print(issue)  # repsonse: MAF-4768
+    print(issue.fields.project.key)  # 'MAF'
+    print(issue.fields.issuetype.name)  # 'User Story'
+    print(issue.fields.reporter.displayName)  # 'Pavel Pokrovskii'
     ##jql = 'project = ' + 'ADCBIUAT' + 'AND issuetype = ' + 'Test' + 'AND STATUS = ' + 'Automated'
-    jql = 'project = ' + 'ADCBIUAT' + ' AND issuetype = ' + 'Test' + ' AND STATUS = ' + 'Automated'
+    jql = (
+        "project = "
+        + "ADCBIUAT"
+        + " AND issuetype = "
+        + "Test"
+        + " AND STATUS = "
+        + "Automated"
+    )
 
     # jql = 'project = ' + project_key + ' AND  worklogDate >= ' + work_date
 
@@ -46,14 +53,18 @@ def main():
     # response like: [<JIRA Issue: key='ADCBIUAT-12005', id='331527'>, ... , <JIRA Issue: key='ADCBIUAT-11918', id='331440'>]
     for i in issues_list:
         print(i)
-# response like:
-# ADCBIUAT-12005
-# ADCBIUAT-12004
-# ADCBIUAT-12001
+        # response like:
+        # ADCBIUAT-12005
+        # ADCBIUAT-12004
+        # ADCBIUAT-12001
 
         issue = jiraNI.issue(i)
-        print('type: {}, assigner: {}'.format(issue.fields.issuetype.name, issue.fields.reporter.displayName))
+        print(
+            "type: {}, assigner: {}".format(
+                issue.fields.issuetype.name, issue.fields.reporter.displayName
+            )
+        )
 
 
-
-if __name__ == '__main__': main()
+if __name__ == "__main__":
+    main()
