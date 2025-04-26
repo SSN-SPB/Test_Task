b6d1089c5f89f50da8a71ef7df96e57ee6fa609e
Sergei Smirnov6 <sergei_smirnov6@epam.com>
2025-04-26 15:44:24 +0200

    Panda data analyzing2

--- Py_functionality_libs/Py_pandas/py_panda_analyzing/py_panda_analyzing01.py
+++ Py_functionality_libs/Py_pandas/py_panda_analyzing/py_panda_analyzing01.py
@@ -1,6 +1,28 @@
 import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
 
 # Read the existing Excel file
 df = pd.read_excel("..\py_panda_columns_treating\py_columns_init.xlsx", engine="openpyxl")
 print(df.info())  # Check data types and missing values
 print(df.describe())
+
+# missing_data = df.isnull().sum()
+# print(f'Missing data per column:\n{missing_data}')
+#
+# # Visualize missing data with a heatmap
+# sns.heatmap(df.isnull(), cbar=False, cmap='viridis')
+# plt.title('Missing Data Visualization')
+# plt.show()
+#
+# plt.scatter(df['Day1'], df['Day3'])
+# plt.title('Scatter plot of column1 vs column2')
+# plt.show()
+empty_cells = df.isna()
+
+# Loop to show addresses
+for row_idx, col_name in zip(*empty_cells.to_numpy().nonzero()):
+    print(f"Empty cell at Row {row_idx}, Column '{df.columns[col_name]}'")
+
+print(df['Day1'])
