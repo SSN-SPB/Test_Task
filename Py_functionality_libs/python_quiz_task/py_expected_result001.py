288ca4451c55559ee85537000e52060fd09e8042
Sergei <32073541+SSN-SPB@users.noreply.github.com>
2025-04-30 10:45:12 +0200

    py_quiz list with default value from function (#56)
    Co-authored-by: Sergei Smirnov6 <sergei_smirnov6@epam.com>

---
+++ Py_functionality_libs/python_quiz_task/py_expected_result001.py
@@ -0,0 +1,13 @@
+def func(val,  lst=[]):
+    lst.append(val)
+    return lst
+
+
+def main():
+    print(func(1))
+    print(func(2))
+    print(func(3))
+    assert func(4) == [1, 2, 3, 4]
+
+if __name__ == "__main__":
+    main()
