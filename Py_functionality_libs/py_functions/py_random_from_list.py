fecc5bece8af8a91bfc5ce7e57f5c6169cd6d9e3
Sergei <32073541+SSN-SPB@users.noreply.github.com>
2025-05-06 17:30:20 +0200

    Adding the random.sample example (#58)
    Co-authored-by: Sergei Smirnov6 <sergei_smirnov6@epam.com>

---
+++ Py_functionality_libs/py_functions/py_random_from_list.py
@@ -0,0 +1,15 @@
+import random
+list_to_test = ["x1", "x11", "x12", "x13", "x14", "x15", "x16", "x17",  ]
+
+def return_random_from_list(tested_list, number):
+    return random.sample(tested_list, number)
+
+
+
+def main():
+    retrieved_list = return_random_from_list(list_to_test, 3)
+    print(retrieved_list)
+    assert len(retrieved_list) == 3
+
+if __name__ == "__main__":
+    main()
