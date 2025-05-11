c7f5f5986e291b7f83cb54d277c321150b8f4651
Sergei <32073541+SSN-SPB@users.noreply.github.com>
2025-05-11 16:26:44 +0200

    enumerate element plus code style updating

--- Py_functionality_libs/py_functions/py_func_202_enumerate.py
+++ Py_functionality_libs/py_functions/py_func_202_enumerate.py
@@ -1,5 +1,18 @@
-test_list = [1, 5, 7, 9, 10, 11]
+test_list = [
+    "10a",
+    "11a",
+    "12a",
+    "13a",
+    "14a",
+    "15a",
+    "16a",
+]
 test_dict = dict()
+
+
+def enumerate_list(tested_list):
+    for index, element in enumerate(tested_list):
+        print(f"index: {index}, element: {element}")
 
 
 def main():
@@ -7,6 +20,7 @@ def main():
         print("index: {}, element: {}".format(i, e))
         test_dict[i] = e
     print(test_dict)
+    enumerate_list(test_list)
 
 
 if __name__ == "__main__":
--- Py_functionality_libs/py_functions/py_func_301_itertools_01_accumulate.py
+++ Py_functionality_libs/py_functions/py_func_301_itertools_01_accumulate.py
@@ -5,7 +5,7 @@ import random
 
 # creating list of random length with randomly sorted values
 created_list = list(range(3, random.randint(7, 33)))
-test_list = list(set(created_list[0:: random.randint(2, 7)]))
+test_list = list(set(created_list[0 :: random.randint(2, 7)]))
 
 
 def check_itertools_accumulate(tested_list):
