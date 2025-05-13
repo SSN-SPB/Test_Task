3e7c9ea1fa6fd5845895fa1e3aef76fdf15b68b6
Sergei <32073541+SSN-SPB@users.noreply.github.com>
2025-05-13 17:08:39 +0200

    pytest with allure reports (#64)
    * initial version of allure report for local review is added.
    
    * python random sample
    
    * pytest - two playwright tests with allure report
    
    * pytest - added requirements.txt

--- playwright/tests/test_text_box_page.py
+++ playwright/tests/test_text_box_page.py
@@ -1,4 +1,9 @@
 import pytest
+import allure
+import time
+# pytest --alluredir=allure-results
+# allure generate allure-results --clean -o allure-report
+# tests\allure-report> python -m http.server 8000
 from page_objects.text_box_page import TextBoxPage
 from playwright.sync_api import sync_playwright
 
@@ -18,10 +23,29 @@ def page(browser):
     return page
 
 
-# Test case using the StartingPage object
+@allure.epic("UI")
+@allure.description(
+    """
+    Test suit checks different scenarios at Admin > Request configuration > Request forms
+    for  Custom Form
+    """
+)
+@allure.title("Check demoda/text-box folder")
 def test_login(page):
     text_box_page = TextBoxPage(page)
     text_box_page.navigate("https://demoqa.com/text-box")
 
     # Assertion example - page check
     assert text_box_page.check_loading_page()
+
+
+@allure.title("Check input name")
+def test_fill_name(page):
+    text_box_page = TextBoxPage(page)
+    text_box_page.navigate("https://demoqa.com/text-box")
+    text_box_page.fill_name()
+    text_box_page.submit_input()
+    time.sleep(7)
+
+    # Assertion example - page check
+    assert text_box_page.filled_name_value("test_name")
