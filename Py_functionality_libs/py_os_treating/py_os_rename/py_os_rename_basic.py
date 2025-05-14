1803927718c4f5476889d70d55d26aa69a7fb97f
Sergei Smirnov <ssn.spb@yandex.ru>
2025-05-14 12:22:24 +0200

    os rename functionality

---
+++ Py_functionality_libs/py_os_treating/py_os_rename/py_os_rename_basic.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+import os
+
+def rename_file(file_name_init, file_name_new):
+    os.rename(file_name_init, file_name_new)
+
+
+def main():
+    rename_file("filename.txt", "filename_modified.txt")
+    # rename_file("filename_modified.txt", "filename.txt")
+
+
+if __name__ == '__main__':
+    main()
