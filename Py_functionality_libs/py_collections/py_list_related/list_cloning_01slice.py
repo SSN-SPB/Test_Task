17683870e2dcb2fb69ff0b7f74af8fe61ceabda8
Sergei Smirnov <ssn.spb@yandex.ru>
2025-05-15 17:17:44 +0200

    list sort vs sorted plus code style updating

--- Py_functionality_libs/py_collections/py_list_related/list_cloning_01slice.py
+++ Py_functionality_libs/py_collections/py_list_related/list_cloning_01slice.py
@@ -3,26 +3,27 @@ list1 = [1, 2, 3, 4]
 list2 = [21, 12, 13, 7, 15, 21]
 
 
-def checking_result(expected, received, text="Compare the expected"
-                                             "and the received result"):
+def checking_result(
+    expected, received, text="Compare the expected" "and the received result"
+):
     print(text)
-    print('expected: {}'.format(expected))
-    print('received: {}'.format(received))
+    print("expected: {}".format(expected))
+    print("received: {}".format(received))
     try:
         assert expected == received
-        print('Checking has passed')
+        print("Checking has passed")
     except AssertionError as ae:
         print("checking has failed. \n__doc__: {}".format(ae.__doc__))
         print("__context__: {}".format(ae.__context__))
 
 
 def cloning_list(tested_list):
-    return extend(tested_list)
+    return tested_list
 
 
 def main():
-    checking_result(list1, cloning_list(list1), 'checking cloned list')
-    checking_result(list2, cloning_list(list2), 'checking cloned list')
+    checking_result(list1, cloning_list(list1), "checking cloned list")
+    checking_result(list2, cloning_list(list2), "checking cloned list")
 
 
 if __name__ == "__main__":
--- Py_functionality_libs/py_collections/py_list_related/list_count_elements_03_list_count.py
+++ Py_functionality_libs/py_collections/py_list_related/list_count_elements_03_list_count.py
@@ -3,14 +3,15 @@ list1 = [1, 2, 3, 4]
 list2 = [21, 12, 13, 7, 15, 21]
 
 
-def checking_result(expected, received, text="Compare the expected"
-                                             "and the received result"):
+def checking_result(
+    expected, received, text="Compare the expected" "and the received result"
+):
     print(text)
-    print('expected: {}'.format(expected))
-    print('received: {}'.format(received))
+    print("expected: {}".format(expected))
+    print("received: {}".format(received))
     try:
         assert expected == received
-        print('Checking has passed')
+        print("Checking has passed")
     except AssertionError as ae:
         print("checking has failed. \n__doc__: {}".format(ae.__doc__))
         print("__context__: {}".format(ae.__context__))
@@ -22,8 +23,11 @@ def count_element_in_list(tested_list, element):
 
 def main():
     checked_element = 3
-    checking_result(1, count_element_in_list(list1, checked_element),
-                    'check element {} in list'.format(checked_element))
+    checking_result(
+        1,
+        count_element_in_list(list1, checked_element),
+        "check element {} in list".format(checked_element),
+    )
 
 
 if __name__ == "__main__":
