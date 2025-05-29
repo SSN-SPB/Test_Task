ac15e8b1c895155a4c18dc7e39daaf788b96203c
Sergei Smirnov <ssn.spb@yandex.ru>
2025-05-29 16:51:59 +0200

    js_test_framework_jasmine

---
+++ js_test_frameworks/jasmine/jasmine-ui-test/spec/dummySpec.js
@@ -0,0 +1,7 @@
+// const { JSDOM } = require("jsdom");
+console.log("✅ Loaded spec dummySpec!");
+describe("Dummy test", function () {
+  it("should pass", function () {
+    expect(true).toBe(true);
+  });
+});
