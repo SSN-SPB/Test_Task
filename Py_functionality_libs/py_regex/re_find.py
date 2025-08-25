2da669a77eba6842f700858c45780c79b533bdf7
Sergei <32073541+SSN-SPB@users.noreply.github.com>
2025-08-25 16:55:27 +0200

    Re findall enhanced (#103)
    * re example
    * re example enhanced

--- Py_functionality_libs/py_regex/re_find.py
+++ Py_functionality_libs/py_regex/re_find.py
@@ -1,13 +1,17 @@
 import re
 
 
-test_txt = "You find 1989 and final act is 2025"
+test_txt = "You find 1989 and final now is 2025, yes"
 
 
 def main():
-    fount_text_list = re.findall(r'\d+', test_txt)
-    print(fount_text_list)
-    assert fount_text_list == ['1989', '2025']
+    found_digits = re.findall(r"\d+", test_txt)
+    found_up2_3_letters_words = re.findall(r"\b\w{2,3}\b", test_txt)
+
+    print(found_digits)
+    print(found_up2_3_letters_words)
+    assert found_digits == ["1989", "2025"]
+    assert found_up2_3_letters_words == ["You", "and", "now", "is", "yes"]
 
 
 if __name__ == "__main__":
