436e405b0be9fc85272e58de1dcfc8c8c3caab2f
Sergei Smirnov <ssn.spb@yandex.ru>
2025-09-02 20:23:07 +0200

    collection itertools_chain

--- Py_functionality_libs/py_collections/collections_deque.py
+++ Py_functionality_libs/py_collections/collections_deque.py
@@ -7,12 +7,12 @@ def main():
     d.append(4)
     d.appendleft(-1)
     d.appendleft("x")
-    print(list_test)   # (1, 2, 3)
-    print(d)           # deque(['x', -1, 1, 2, 3, 4])
-    print(type(d))     # <class 'collections.deque'>
+    print(list_test)  # (1, 2, 3)
+    print(d)  # deque(['x', -1, 1, 2, 3, 4])
+    print(type(d))  # <class 'collections.deque'>
     print(list_test)
     list_test = list(d)
-    print(list_test)   # ['x', -1, 1, 2, 3, 4]
+    print(list_test)  # ['x', -1, 1, 2, 3, 4]
 
 
 if __name__ == "__main__":
