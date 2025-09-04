68025dd60145e9e7379115ceadf5d6361801f2e2
Sergei Smirnov <ssn.spb@yandex.ru>
2025-09-04 18:58:56 +0200

    list compression

--- Py_functionality_libs/py_collections/collections_list_compression.py
+++ Py_functionality_libs/py_collections/collections_list_compression.py
@@ -6,9 +6,9 @@
 def main():
     list1 = [x**2 for x in [1, 2, 3]]
     assert list1 == [1, 4, 9]
-    list2 = [x*3 for x in ["a", "b"]]
+    list2 = [x * 3 for x in ["a", "b"]]
     assert list2 == ["aaa", "bbb"]
-    list3 = [x*3 for x in ("a", "b")]
+    list3 = [x * 3 for x in ("a", "b")]
     assert list3 == list2
     list5 = [x**2 for x in range(1, 4)]
     assert list5 == [1, 4, 9]
