11bf7a844d177e7061ea97886c3de89c900db049
Sergei Smirnov <ssn.spb@yandex.ru>
2025-09-05 19:30:31 +0200

    collections_deque

--- Py_functionality_libs/py_collections/collections_deque.py
+++ Py_functionality_libs/py_collections/collections_deque.py
@@ -4,11 +4,11 @@ from collections import deque
 def main():
     list_test = (1, 2, 3)
     d = deque(list_test)
-    print(f"initial deque from set: {d}") # ([1, 2, 3])
+    print(f"initial deque from set: {d}")  # ([1, 2, 3])
     d.append(4)
-    print(f" deque after append: {d}") #  ([1, 2, 3, 4])
+    print(f" deque after append: {d}")  # ([1, 2, 3, 4])
     d.appendleft(-1)
-    print(f" deque after appendleft -1: {d}") #  ([1, 2, 3, 4])
+    print(f" deque after appendleft -1: {d}")  # ([1, 2, 3, 4])
     print(list_test)  # (1, 2, 3)
     print(d)  # deque(['x', -1, 1, 2, 3, 4])
     print(type(d))  # <class 'collections.deque'>
