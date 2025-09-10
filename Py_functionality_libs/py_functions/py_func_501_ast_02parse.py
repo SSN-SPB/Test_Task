edce5d25648f3a58b0cf6f05efb142fe4c0fbfeb
Sergei <32073541+SSN-SPB@users.noreply.github.com>
2025-09-10 20:44:34 +0200

    py_func_501_ast_02parse (#113)
    * ast literal parse dump

---
+++ Py_functionality_libs/py_functions/py_func_501_ast_02parse.py
@@ -0,0 +1,25 @@
+import ast
+
+source_code = "a + b * 2"
+
+# Parse the code into an AST
+tree = ast.parse(source_code, mode='eval')
+
+
+def main():
+    print(ast.dump(tree, indent=4))
+
+
+if __name__ == "__main__":
+    main()
+
+#
+# Output:
+# Expression(
+#     body=BinOp(
+#         left=Name(id='a', ctx=Load()),
+#         op=Add(),
+#         right=BinOp(
+#             left=Name(id='b', ctx=Load()),
+#             op=Mult(),
+#             right=Constant(value=2))))
