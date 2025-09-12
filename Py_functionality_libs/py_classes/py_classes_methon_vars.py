e4f988aebf53d92a2f72e3de03a9699c5c50ed33
Sergei Smirnov <ssn.spb@yandex.ru>
2025-09-12 19:54:57 +0200

    vars of class

---
+++ Py_functionality_libs/py_classes/py_classes_methon_vars.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+
+class Counter:
+    count = 5
+
+    def __init__(self):
+        self.count = 7
+
+    @classmethod
+    def count_increase(cls, number):
+        result = cls.count + number
+        # print(result)
+        return result
+
+    def get_count(self):
+        # print(self.value)
+        return self.count
+
+
+def main():
+    s = Counter()
+    print(vars(s))
+    assert vars(s) == {'count': 7}
+
+if __name__ == "__main__":
+    main()
