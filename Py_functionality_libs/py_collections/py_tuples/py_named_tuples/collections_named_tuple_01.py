4cf312c0d5fbff04173585efdd6176962d9af64b
Sergei Smirnov <ssn.spb@yandex.ru>
2025-09-15 16:37:26 +0200

    namedtuple example

---
+++ Py_functionality_libs/py_collections/py_tuples/py_named_tuples/collections_named_tuple_01.py
@@ -0,0 +1,22 @@
+# Access by name
+# Field names
+
+from collections import namedtuple
+
+
+def named_tuple_example():
+    Point = namedtuple("Point", ["x", "y"])
+    point = Point(10, 20)
+    print(point.x)
+    print(point.y)
+    assert point.x == 10
+    assert point.y == 20
+
+
+
+def main2():
+    named_tuple_example()
+
+
+if __name__ == "__main__":
+    main2()
