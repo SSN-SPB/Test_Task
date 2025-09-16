d5498a4d56dcb24029aafad37aed4bbb6da71cc8
Sergei Smirnov <ssn.spb@yandex.ru>
2025-09-16 16:25:21 +0200

    uuid and uuid3 enhanced

--- Py_functionality_libs/py_functions/py_func_502_uuid.py
+++ Py_functionality_libs/py_functions/py_func_502_uuid.py
@@ -1,12 +1,14 @@
 import uuid
 
 namespace = uuid.NAMESPACE_DNS
+namespace2 = uuid.NAMESPACE_URL
 name = "example.com"
 
 
 def main():
     print(uuid.uuid4())
     print(uuid.uuid3(namespace, name))
+    print(uuid.uuid3(namespace2, name))
     # uuid3 is always the same
     assert uuid.uuid3(namespace, name) == uuid.uuid3(namespace, name)
     assert str(uuid.uuid3(namespace, name)) == "9073926b-929f-31c2-abc9-fad77ae3e8eb"
