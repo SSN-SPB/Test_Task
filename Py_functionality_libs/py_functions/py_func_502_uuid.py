a85d8625c67638affc9343ec85848d40e3e18afb
Sergei <32073541+SSN-SPB@users.noreply.github.com>
2025-10-03 18:30:47 +0200

    enumerate that starts with index (#126)

--- Py_functionality_libs/py_functions/py_func_502_uuid.py
+++ Py_functionality_libs/py_functions/py_func_502_uuid.py
@@ -11,7 +11,8 @@ def main():
     print(uuid.uuid3(namespace2, name))
     # uuid3 is always the same
     assert uuid.uuid3(namespace, name) == uuid.uuid3(namespace, name)
-    assert str(uuid.uuid3(namespace, name)) == "9073926b-929f-31c2-abc9-fad77ae3e8eb"
+    test_value = uuid.uuid3(namespace, name)
+    assert str(test_value) == "9073926b-929f-31c2-abc9-fad77ae3e8eb"
 
 
 if __name__ == "__main__":
