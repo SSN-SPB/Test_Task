423e33c1870f27769e64069c1da2f8709c6fd142
Sergei <32073541+SSN-SPB@users.noreply.github.com>
2025-11-05 20:52:59 +0100

    Merge pull request #133
    * python random_sample

--- Py_functionality_libs/py_functions/py_random/py_random_sample.py
+++ Py_functionality_libs/py_functions/py_random/py_random_sample.py
@@ -1,15 +1,17 @@
-import random
+from random import sample
 
-init_list = ["x1", "x2", "x3", "x4", "x5", "x6", "x7"]
+init_list = list(map(lambda a: a + a + a, range(0, 10, 2)))
 
 
-def random_sample(tested_list, x):
-    return random.sample(tested_list, x)
+def random_sample(tested_collection, x):
+    return sample(tested_collection, x)
 
 
 def main():
+    print(init_list)
     print(random_sample(init_list, 3))
     assert len(random_sample(init_list, 3)) == 3
+    assert type(random_sample(init_list, 3)) == type(init_list)
 
 
 if __name__ == "__main__":
