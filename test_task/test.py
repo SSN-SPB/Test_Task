478c88e9f4c8b08ba1af242a1365c56b3f527948
Sergei Smirnov <ssn.spb@yandex.ru>
2025-11-05 20:52:08 +0100

    python random_sample

--- test_task/test.py
+++ test_task/test.py
@@ -1,5 +1,17 @@
-a = '123'
-b = '456'
-c = a + b
-print(c)
-print(c == '123456')
+import cProfile
+
+def my_function():
+    a = 1
+    b = 1
+    assert a == b
+    pass
+
+cProfile.run('my_function()')
+
+
+def main():
+    cProfile.run('my_function()')
+
+
+if __name__ == "__main__":
+    main()
