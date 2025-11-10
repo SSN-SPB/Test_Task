6cceb2f2007989424e38335d95f6ef34741353a1
Sergei <32073541+SSN-SPB@users.noreply.github.com>
2025-11-10 17:17:41 +0100

    Merge pull request #135
    * python itertools pairwise
    
    * python itertools pairwise code style updating

---
+++ Py_functionality_libs/py_iterations/py_iterations_05_pairwise.py
@@ -0,0 +1,14 @@
+from itertools import pairwise
+
+
+def main2():
+    list2 = [1, 2, 3, 6, 12]
+    dict_test = dict()
+    for a, b in pairwise(list2):
+        print(f"{a}-{b}")
+        dict_test[a] = b
+    print(dict_test)
+
+
+if __name__ == "__main__":
+    main2()
