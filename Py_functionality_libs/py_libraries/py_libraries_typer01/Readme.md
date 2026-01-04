ca166e93e99b27f03c7ad4077923ea52eb4f31f3
Sergei Smirnov <ssn.spb@yandex.ru>
2026-01-04 13:25:27 +0100

    pytest libraries typer version 01

---
+++ Py_functionality_libs/py_libraries/py_libraries_typer01/Readme.md
@@ -0,0 +1,8 @@
+<h1>Example of CL to run</h1>
+<br>
+python typer_example.py goodbye Smith --no-formal <br>
+<br>
+python typer_example.py goodbye Smith --formal<br>
+<br>
+python typer_example.py hello Bob<br>
+<br>
---
+++ Py_functionality_libs/py_libraries/py_libraries_typer01/typer_example.py
@@ -0,0 +1,19 @@
+import typer
+
+app = typer.Typer()
+
+@app.command()
+def hello(name: str):
+    # Приветствие пользователя
+    print(f"Hello {name}")
+
+@app.command()
+def goodbye(name: str, formal: bool = False):
+    # Прощание с пользователем
+    if formal:
+        print(f"Goodbye Ms./Mr. {name}. Have a good day.")
+    else:
+        print(f"Bye {name}!")
+
+
+if __name__ == "__main__": app()
