9d4e80d4de919e9245aea6174e4037e8e9da56ff
Sergei Smirnov <ssn.spb@yandex.ru>
2026-01-20 19:16:45 +0100

    pytorch 006 numpy with matplotlib color tensor

--- Py_functionality_libs/py_11_matplotlib/py_matplotlib_002_with_tensor.py
+++ Py_functionality_libs/py_11_matplotlib/py_matplotlib_002_with_tensor.py
@@ -6,16 +6,15 @@ def main():
     # Create 28*28 grayscale image tensor
     image = torch.rand(28, 56)
     # Convert to Numpy for plotting
-    selected_color = 'managua'
+    selected_color = "managua"
 
-    plt.imshow(image.numpy(),cmap=selected_color)
+    plt.imshow(image.numpy(), cmap=selected_color)
     print(f"All possible colors: {plt.colormaps()}")
     # plt.imshow(image.numpy(),cmap='gray')
     plt.title(f"{selected_color}Scale tensor")
     plt.grid("Grid enabled")
     plt.axis("on")
     plt.show()
-
 
 
 if __name__ == "__main__":
