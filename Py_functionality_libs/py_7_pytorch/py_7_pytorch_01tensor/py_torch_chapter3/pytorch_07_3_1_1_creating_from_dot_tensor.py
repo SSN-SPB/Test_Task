0a6a7425fae71c058f625344f01a3cfeff5d90e9
Sergei Smirnov <ssn.spb@yandex.ru>
2026-01-27 20:09:06 +0100

    chapter 3.1 - creating tensor several ways

---
+++ Py_functionality_libs/py_7_pytorch/py_7_pytorch_01tensor/py_torch_chapter3/pytorch_07_3_1_1_creating_from_dot_tensor.py
@@ -0,0 +1,15 @@
+import torch
+
+data_test = [[1,2], [3,4]]
+def main():
+    # Create tensor
+    tensor_1 = torch.tensor(data_test)
+    print(f"tensor1: {tensor_1}")
+
+# Output
+# tensor1: tensor([[1, 2],
+#         [3, 4]])
+
+
+if __name__ == "__main__":
+    main()
