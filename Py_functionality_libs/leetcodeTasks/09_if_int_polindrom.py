8fdd6b2d18d57b1afe54f3b5c107a5844750ef74
Sergei Smirnov <ssn.spb@yandex.ru>
2026-01-29 13:48:55 +0100

    leetcode 09 version 2 and updated version 1

--- Py_functionality_libs/leetcodeTasks/09_if_int_polindrom.py
+++ Py_functionality_libs/leetcodeTasks/09_if_int_polindrom.py
@@ -1,7 +1,4 @@
-# https://leetcode.com/problems/reverse-integer/description/ 10
-
-import datetime
-from datetime import datetime as dt
+# https://leetcode.com/problems/palindrome-number/description/ 09
 
 
 def isPalindrome(x: int) -> bool:
@@ -10,9 +7,9 @@ def isPalindrome(x: int) -> bool:
 
 
 def main():
-    assert isPalindrome(26) == False
-    assert isPalindrome(121) == True
-    assert isPalindrome(-121) == False
+    assert not isPalindrome(26)
+    assert isPalindrome(121)
+    assert not isPalindrome(-121)
 
 
 if __name__ == "__main__":
