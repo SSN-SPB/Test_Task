4ed2f03c3d7c58df8bd179d4d6048ed191c60ea8
Sergei Smirnov <ssn.spb@yandex.ru>
2026-01-30 19:23:44 +0100

    7_3_1_3 datatype control

--- Py_functionality_libs/py_7_pytorch/py_7_pytorch_01tensor/py_torch_chapter3/pytorch_07_3_1_1_creating_from_dot_tensor.py
+++ Py_functionality_libs/py_7_pytorch/py_7_pytorch_01tensor/py_torch_chapter3/pytorch_07_3_1_1_creating_from_dot_tensor.py
@@ -1,10 +1,13 @@
 import torch
 
-data_test = [[1,2], [3,4]]
+data_test = [[1, 2], [3, 4]]
+
+
 def main():
     # Create tensor
     tensor_1 = torch.tensor(data_test)
     print(f"tensor1: {tensor_1}")
+
 
 # Output
 # tensor1: tensor([[1, 2],
