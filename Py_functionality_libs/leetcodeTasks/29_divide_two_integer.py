5bb62bd57ff75c2f6f964d2f06c4b55b9c1314a7
Sergei Smirnov <ssn.spb@yandex.ru>
2026-02-25 13:37:28 +0100

    Leetcode 29 divide two integers. passed - simplified refactored

---
+++ Py_functionality_libs/leetcodeTasks/29_divide_two_integer.py
@@ -0,0 +1,26 @@
+# https://leetcode.com/problems/divide-two-integers/
+# version 2026-Feb-25 passed
+import math
+
+
+class Solution(object):
+    def divide(self, dividend, divisor):
+        """
+        :type dividend: int
+        :type divisor: int
+        :rtype: int
+        """
+        condition_one = dividend >= 0 and divisor < 0
+        condition_two = dividend < 0 and divisor >= 0
+        sign = -1 if condition_one or condition_two else 1
+        return min(max(int(abs(dividend)/abs(divisor)) * sign, -2 ** 31), 2 ** 31-1)
+
+
+def main():
+    c = Solution()
+    print(f" result of division is : {c.divide(7, -3)}")
+    assert c.divide(7, -3) == -2
+
+
+if __name__ == "__main__":
+    main()
