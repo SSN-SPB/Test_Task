d8649f2917e5436888f0aea41c0bf801ae4e92e1
Sergei Smirnov <ssn.spb@yandex.ru>
2026-03-02 17:20:35 +0100

    leetcode 30 substring-with-concatenation-of-all-words

---
+++ Py_functionality_libs/leetcodeTasks/30_all_words_concatanations.py
@@ -0,0 +1,42 @@
+# https://leetcode.com/problems/substring-with-concatenation-of-all-words/
+# version 2026-Mar-02 in progress
+from itertools import permutations
+
+words_to_concatanate = ["foo", "bar"]
+checked_string = "barfoothefoobarman"
+
+
+class Solution(object):
+    def findSubstring(self, s, words):
+        """
+        :type s: str
+        :type words: List[str]
+        :rtype: List[int]
+        """
+        result_list = []
+        if len(s) == 1:
+            return result_list
+        if len(set(words)) == 1:
+            return [0]
+
+        concatenation_list = list(permutations(words))
+        concatenation_strings = []
+        for x in range(0, len(concatenation_list)):
+            concatenation_strings.append("".join(concatenation_list[x]))
+        for y in range(0, len(s)):
+            if y + len(concatenation_strings[0]) <= len(s):
+                if s[y : y + len(concatenation_strings[0])] in concatenation_strings:
+                    result_list.append(y)
+        return result_list
+
+
+def main():
+    c = Solution()
+    print(
+        f" result of division is : {c.findSubstring(checked_string, words_to_concatanate)}"
+    )
+    assert c.findSubstring(checked_string, words_to_concatanate) == [0, 9]
+
+
+if __name__ == "__main__":
+    main()
