421e59b00592793e052911c691a628873997784f
Sergei Smirnov <ssn.spb@yandex.ru>
2026-03-12 15:57:22 +0100

    LeetCode 32 is in progress

--- Py_functionality_libs/leetcodeTasks/32_longest_valid_parentheses.py
+++ Py_functionality_libs/leetcodeTasks/32_longest_valid_parentheses.py
@@ -2,9 +2,10 @@
 # version 2026-Mar-10
 
 
-tested_string = ["(()", 2]
-# tested_string = ["(()())", 4]
+tested_strings = [["(()", 2],[")()())", 4], ["", 0]]
+# tested_string = ["(()", 2]
 # tested_string = [")()())", 4]
+tested_string = ["", 0]
 # tested_string = [")(",0]
 
 class Solution(object):
@@ -20,25 +21,27 @@ class Solution(object):
         if tested_length == 1:
             return result
         for x in range(0, tested_length):
-            print(s[x])
             if s[x] == direct and s[x + 1] == reversed:
                 result += 1
                 x += 2
                 continue
             if s[x] == reversed:
-                tested_length -= 2
-                break
-            if s[x] == direct and s[len(s) - 1 - x] == reversed:
-                result += 1
-                tested_length -= 2
-                break
+                # tested_length -= 2
+                continue
+            # if s[x] == direct and s[len(s) - 1 - x] == reversed:
+            #     result += 1
+            #     tested_length -= 2
+            #     continue
         return result * 2
 
 
 def main():
     c = Solution()
-    print(f" result : {c.longestValidParentheses(tested_string[0])}")
-    assert c.longestValidParentheses(tested_string) == tested_string[1]
+    # print(f" result : {c.longestValidParentheses(tested_string[0])}")
+    # assert c.longestValidParentheses(tested_string[0]) == tested_string[1]
+    for x in range(0, len(tested_strings)):
+        print(f" result : {c.longestValidParentheses(tested_strings[x][0])}")
+        assert c.longestValidParentheses(tested_strings[x][0]) == tested_strings[x][1]
 
 
 if __name__ == "__main__":
