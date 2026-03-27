2ba5502b5ee967f4e517de8979b821a2e8663668
Sergei Smirnov <ssn.spb@yandex.ru>
2026-03-27 14:12:57 +0100

    restore without commited js

--- jasmine_taf_ui/data/textBoxData.js
+++
@@ -1,6 +0,0 @@
-{
-  "name": "John Doe",
-  "email": "john@example.com",
-  "currentAddress": "123 Current St",
-  "permanentAddress": "456 Permanent Ave"
-}
--- js_test_frameworks/jasmine/jasmine-ui-test/spec/dummySpec.js
+++
@@ -1,7 +0,0 @@
-// const { JSDOM } = require("jsdom");
-console.log("✅ Loaded spec dummySpec!");
-describe("Dummy test", function () {
-  it("should pass", function () {
-    expect(true).toBe(true);
-  });
-});
