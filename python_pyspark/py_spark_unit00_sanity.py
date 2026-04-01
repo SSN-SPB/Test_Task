8583142acd49dc591cf331b6b1128f35e6edf5f5
Sergei Smirnov <ssn.spb@yandex.ru>
2026-04-01 16:01:21 +0200

    Minor updating python scripts

--- python_pyspark/py_spark_unit00_sanity.py
+++ python_pyspark/py_spark_unit00_sanity.py
@@ -1,3 +1,9 @@
+# This is a simple sanity check to ensure that PySpark is working correctly.
+# PySpark is a powerful tool for big data processing, and this code
+# will create a Spark session, generate a range of numbers,
+# and display them to confirm that everything is set up properly.
+# If you see the output of the range and the message "Spark is working!",
+# then your PySpark environment is ready to use.
 from pyspark.sql import SparkSession
 
 spark = SparkSession.builder.appName("test").getOrCreate()
