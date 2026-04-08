b3d966a866fce0608a91fbd11191555f2856693c
Sergei Smirnov <ssn.spb@yandex.ru>
2026-04-08 18:29:58 +0200

    python file reading enhanced example

---
+++ Py_functionality_libs/py_fileTreating/py_to_file_02_reading.py
@@ -0,0 +1,18 @@
+def main():
+    print("Hello File")
+    # creating new file; "w+" the access level to work with new file
+    # w+ - overwriting
+    # r - reading
+    # a - append to the end of file
+    # x - open for exclusive creation, failing if the file already exists
+    # b - byte
+    # e.g. 'r+b'
+    # .flush() - cleans buffer
+    # .close() - closes file and cleans buffer
+    new_file_name = open("output.txt", "r+")
+    print(dir(new_file_name))
+    for line in new_file_name:
+        print(line.strip())
+
+if __name__ == "__main__":
+    main()
