b29eb2831e6bbfe9a5458edfa627a555a4e5e3d7
Sergei Smirnov <ssn.spb@yandex.ru>
2026-04-09 20:56:33 +0200

    python file reading enhanced example with async

--- Py_functionality_libs/py_functions/py_func_901_asyncio_basic05_filetreating.py
+++ Py_functionality_libs/py_functions/py_func_901_asyncio_basic05_filetreating.py
@@ -1,28 +1,38 @@
 # This is a basic example of using the asyncio
 # library in Python to run asynchronous tasks.
 import asyncio
-import aiohttp
+import random
+
+random_values = [random.randint(1, 100) for _ in range(1000)]
 
 
-urls = [
-    "https://jsonplaceholder.typicode.com/posts/1",
-    "https://jsonplaceholder.typicode.com/posts/2",
-    "https://jsonplaceholder.typicode.com/posts/3",
-]
+async def file_creating(initial_list):
+    await asyncio.sleep(5)  # Emulate delay before creating the file
+    print("Creating file with random values...")
+    # print(initial_list)
+    with open("output.txt", "w") as file:
+        for value in initial_list:
+            file.write(f"Value for {initial_list.index(value)} is {value}\n")
 
-async def fetch(session, url):
-    async with session.get(url) as response:
-        data = await response.json()
-        return data
+    print("File created with random values.")
+    await asyncio.sleep(17)
+
+
+async def file_reading(file_name):
+    with open(file_name, "r") as new_file_name:
+        for line in new_file_name:
+            print(line.strip())
+    # try:
+    #     with open(file_name, "r") as new_file_name:
+    #         for line in new_file_name:
+    #             print(line.strip())
+    # except FileNotFoundError:
+    #     print(f"File {file_name} not found.")
+
 
 async def main():
-    async with aiohttp.ClientSession() as session:
-        tasks = [fetch(session, url) for url in urls]
-        results = await asyncio.gather(*tasks)
-
-        for result in results:
-            print(f'Title of result.id {result["id"]}: {result["title"]}')
-
+    await file_creating(random_values)
+    await file_reading("output.txt")
 
 
 if __name__ == "__main__":
