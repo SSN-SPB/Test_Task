c9b489a03891abfd300c5e5cdd37f1844f516992
Sergei Smirnov <ssn.spb@yandex.ru>
2026-04-22 11:42:53 +0200

    python os vs path

---
+++ Py_functionality_libs/py_fileTreating/py_os_vs_path_glob.py
@@ -0,0 +1,22 @@
+import os.path
+from pathlib import Path
+
+def main():
+    file_path = os.path.join('data', 'users', 'config.json')
+    parent_dir = os.path.dirname(file_path)
+    file_name = os.path.basename(file_path)
+    print(f"Parent directory: {parent_dir}")
+    print(f"File name: {file_name}")
+    print(f"File name: {file_path}")
+
+    # use pathlib
+    file_path = Path('data') / 'users' / 'config.json'
+    parent_dir = file_path.parent
+    file_name = file_path.name
+
+    print(f"Parent directory Path: {parent_dir}")
+    print(f"File name Path: {file_name}")
+    print(f"File path Path: {file_path}")
+
+if __name__ == "__main__":
+    main()
