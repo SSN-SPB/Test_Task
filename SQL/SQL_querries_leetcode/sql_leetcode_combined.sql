58a94bf4ed60b213d6b885088661c2f3daaf083a
Sergei Smirnov <ssn.spb@yandex.ru>
2026-05-09 17:11:11 +0200

    SQL LEETCODE 577

--- SQL/SQL_querries_leetcode/sql_leetcode_combined.sql
+++ SQL/SQL_querries_leetcode/sql_leetcode_combined.sql
@@ -271,5 +271,37 @@ GROUP BY A.PLAYER_ID
 --Each row of this table indicates the id of a customer, their name, and the id of the customer who referred them.
 SELECT C.NAME FROM CUSTOMER C WHERE C.REFEREE_ID IS NULL OR C.REFEREE_ID != 2
 
+--577 https://leetcode.com/problems/employee-bonus/description/ passed 9-MAY-2026
+-- Table: Employee
+-- +-------------+---------+
+-- | Column Name  | Type    |
+-- +-------------+---------+
+-- | id          | int     |
+-- | name        | varchar |
+-- | salary      | int     |
+-- | departmentId | int     |
+-- +-------------+---------+
+-- id is the primary key for this table.
+--Table: Bonus
+--
+--+-------------+------+
+--| Column Name | Type |
+--+-------------+------+
+--| empId       | int  |
+--| bonus       | int  |
+--+-------------+------+
+--empId is the column of unique values for this table.
+--empId is a foreign key (reference column) to empId from the Employee table.
+--Each row of this table contains the id of an employee and their respective bonus.
+--
+--Write a solution to report the name and bonus amount of each employee who satisfies either of the following:
+--
+--The employee has a bonus less than 1000.
+--The employee did not get any bonus.
+--Return the result table in any order.
+
+SELECT E.NAME, B.BONUS
+    FROM EMPLOYEE E LEFT JOIN BONUS B ON E.EMPID = B.EMPID
+        WHERE B.BONUS < 1000 OR B.BONUS IS NULL
 
 
