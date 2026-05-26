0797ffc4ee7bbebfc046cf17fa5bce3cae8b0c0b
Sergei Smirnov <ssn.spb@yandex.ru>
2026-05-26 11:17:39 +0200

    ternary operator example

---
+++ Py_functionality_libs/py_functions/py_func_503_01_terner_operation.py
@@ -0,0 +1,13 @@
+# ternary operator
+# It is not recommended for inclosure
+
+a = 9
+
+
+def main():
+    result = "Odd" if a % 2 != 0 else "Even"
+    print(result)
+
+
+if __name__ == "__main__":
+    main()
