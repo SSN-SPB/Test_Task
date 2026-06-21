38623011555d7d29976e1eae43cee8643317e06d
SSN-SPB <smirnov.s68@mail.ru>
2026-06-21 14:22:02 +0200

    leetcode SQL Tree Node

--- SQL/SQL_querries_leetcode/sql_leetcode_combined.sql
+++ SQL/SQL_querries_leetcode/sql_leetcode_combined.sql
@@ -531,3 +531,28 @@ WHERE T.ID % 2 <> 0
 ORDER BY t.ID DESC
 LIMIT 1;
 
+608 Tree Node https://leetcode.com/problems/tree-node/submissions/2040961463/
+
+Table: Tree
+
++-------------+------+
+| Column Name | Type |
++-------------+------+
+| id          | int  |
+| p_id        | int  |
++-------------+------+
+id is the column with unique values for this table.
+Each row of this table contains information about the id of a node and the id of its parent node in a tree.
+The given structure is always a valid tree.
+
+
+Each node in the tree can be one of three types:
+
+"Leaf": if the node is a leaf node.
+"Root": if the node is the root of the tree.
+"Inner": If the node is neither a leaf node nor a root node.
+Write a solution to report the type of each node in the tree.
+
+SELECT T.id, IF(T.P_ID is Null,"Root",
+                IF(T.id in (SELECT DISTINCT(TR.P_ID) AS P FROM TREE TR), "Inner", "Leaf")
+                ) AS type from Tree T
