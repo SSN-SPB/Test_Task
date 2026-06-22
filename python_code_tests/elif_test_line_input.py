e443d63336600e72bd4e4706fd1970a61d3df831
SSN-SPB <smirnov.s68@mail.ru>
2026-06-22 19:43:42 +0200

    minor comments enhancement

--- python_code_tests/elif_test_line_input.py
+++ python_code_tests/elif_test_line_input.py
@@ -1,3 +1,11 @@
+# https://hr.gs/automationskills sample
+# this is a simple fizzbuzz program that
+# takes an integer input and prints "Fizz"
+# for multiples of 3, "Buzz" for multiples
+# of 5, and "FizzBuzz" for multiples of both
+# 3 and 5. For all other numbers, it prints the number itself.
+
+
 def fizzBuzz(n):
     for i in range(1, n + 1):
         if i % 3 == 0 and i % 5 == 0:
@@ -11,7 +19,7 @@ def fizzBuzz(n):
             # Write your code here
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     n = int(input("Input integer value: ").strip())
 
     fizzBuzz(n)
