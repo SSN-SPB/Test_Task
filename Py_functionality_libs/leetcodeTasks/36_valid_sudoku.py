2ba158e398014308f3df2060bd688cb418f0d3b8
SSN-SPB <smirnov.s68@mail.ru>
2026-07-06 11:43:44 +0200

    python leetcode 36 - partly

--- Py_functionality_libs/leetcodeTasks/36_valid_sudoku.py
+++ Py_functionality_libs/leetcodeTasks/36_valid_sudoku.py
@@ -1,15 +1,16 @@
 # https://leetcode.com/problems/valid-sudoku/description/
-# 36. Valid Sudoku completed partly 26-jul-6
+# 36. Valid Sudoku passed 26-jul-6
 board_list = [
- ["5","3",".",".","7",".",".",".","."]
-,["6",".",".","1","9","5",".",".","."]
-,[".","9","8",".",".",".",".","6","."]
-,["8",".",".",".","6",".",".",".","3"]
-,["4",".",".","8",".","3",".",".","1"]
-,["7",".",".",".","2",".",".",".","6"]
-,[".","6",".",".",".",".","2","8","."]
-,[".",".",".","4","1","9",".",".","5"]
-,[".",".",".",".","8",".",".","7","9"]]
+    ["5", "3", ".", ".", "7", ".", ".", ".", "."],
+    ["6", ".", ".", "1", "9", "5", ".", ".", "."],
+    [".", "9", "8", ".", ".", ".", ".", "6", "."],
+    ["8", ".", ".", ".", "6", ".", ".", ".", "3"],
+    ["4", ".", ".", "8", ".", "3", ".", ".", "1"],
+    ["7", ".", ".", ".", "2", ".", ".", ".", "6"],
+    [".", "6", ".", ".", ".", ".", "2", "8", "."],
+    [".", ".", ".", "4", "1", "9", ".", ".", "5"],
+    [".", ".", ".", ".", "8", ".", ".", "7", "9"],
+]
 
 
 class Solution(object):
@@ -18,7 +19,7 @@ class Solution(object):
         :type board: List[List[str]]
         :rtype: bool
         """
-        sample_list = ['1','2','3','4','5','6','7','8','9']
+        sample_list = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
         # append vertical lists
         for v in range(0, 9):
             interim_list = []
@@ -26,16 +27,26 @@ class Solution(object):
                 interim_list.append(board[h][v])
             # print(interim_list)
             board.append(interim_list)
+        # append block lists
+        for i in range(0, 9, 3):
+            for j in range(0, 9, 3):
+                interim_block = []
+                interim_block.append(board[i][j])
+                interim_block.append(board[i + 1][j])
+                interim_block.append(board[i + 2][j])
+                interim_block.append(board[i][j + 1])
+                interim_block.append(board[i + 1][j + 1])
+                interim_block.append(board[i + 2][j + 1])
+                interim_block.append(board[i][j + 2])
+                interim_block.append(board[i + 1][j + 2])
+                interim_block.append(board[i + 2][j + 2])
+                board.append(interim_block)
         for w in board:
-            # print(w)
+            print(w)
             for x in sample_list:
-                print(f"check {w}")
-                print(f"{x} - counted {w.count(x)}")
                 if w.count(x) > 1:
                     return False
-
         return True
-
 
 
 def main():
