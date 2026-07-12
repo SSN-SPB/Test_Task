75f5c80d46f92e148bc8b34ead07815c43ab2992
SSN-SPB <smirnov.s68@mail.ru>
2026-07-12 18:16:33 +0200

    js tutorial - equals and input

---
+++ js/js_code_practice/13js_misc/13js_misc_01_equal.js
@@ -0,0 +1,19 @@
+console.log('5 == "5"'); // true
+console.log(5 == "5"); // true
+console.log(5 === "5"); // false
+
+console.log("false == 0"); // true
+console.log(false == 0); // true
+console.log(false === 0); // false
+
+console.log("true == 1"); // true
+console.log(true == 1); // true
+console.log(true === 1); // false
+
+console.log("[] == false"); // true
+console.log([] == false); // true
+console.log([] === false); // false
+
+console.log("null == undefined"); // true
+console.log(null == undefined); // true
+console.log(null === undefined); // false
