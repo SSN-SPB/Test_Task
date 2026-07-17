b9366b7668a5f21f9ab5f3914ae25038b9c8ac5a
SSN-SPB <smirnov.s68@mail.ru>
2026-07-17 14:14:05 +0200

    python regex search groups

--- Py_functionality_libs/py_regex/py_regex_search/py_regex_search_03group.py
+++ Py_functionality_libs/py_regex/py_regex_search/py_regex_search_03group.py
@@ -1,1 +1,18 @@
 # re.search by group
+import re
+
+from service_packages.service_logger.logger_provider import logger
+
+TEST_PHONE_NUMBER = "123-22-186-11"
+SEARCH_PATTERN = r"(\d{3})-(\d{2})-(\d{3})-(\d{2})"
+
+
+def main():
+    checked = re.search(SEARCH_PATTERN, TEST_PHONE_NUMBER)
+    for i in range(1, len(checked.groups()) + 1):
+        logger.info(checked.group(i))
+    logger.info(checked.groups())
+
+
+if __name__ == "__main__":
+    main()
