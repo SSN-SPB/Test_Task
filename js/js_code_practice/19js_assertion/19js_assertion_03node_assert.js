26208046c6a4b0028e9441107ea7c1be7b189041
SSN-SPB <smirnov.s68@mail.ru>
2026-08-09 13:49:49 +0200

    js node assert example

--- js/js_code_practice/19js_assertion/19js_assertion_03node_assert.js
+++ js/js_code_practice/19js_assertion/19js_assertion_03node_assert.js
@@ -12,6 +12,10 @@ console.log(checkStringLength(newString));
 
 try {
   assert(checkStringLength(newString) > 17);
-} catch {
+} catch (error) {
   console.log("Checking error");
+  console.log(`Error name: ${error.name}`);
+  console.log(`Error message: ${error.message}`);
+  console.log(`Error code: ${error.code}`);
+  // console.log(`Error stack: ${error.stack}`);
 }
