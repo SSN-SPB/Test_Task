14eb993d4490d9281c7c05a2f8219b7da1d2b16f
SSN-SPB <smirnov.s68@mail.ru>
2026-08-17 14:17:24 +0200

    cypress enhancing - function moved to separate folder

--- TAF_test/JS/cypress/cypress_demo03_parameterized/cypress/e2e/parameter_cypress_03_01_basic.cy.js
+++ TAF_test/JS/cypress/cypress_demo03_parameterized/cypress/e2e/parameter_cypress_03_01_basic.cy.js
@@ -1,9 +1,11 @@
 import { testData } from "../../serviceData/dataToTest.js";
+import { sumOfTwo } from "../../testedFunctions/testedFunctions_sums.js";
 
 describe("Parameterized Cypress test to check method that sums two integers", () => {
   testData.forEach(({ x, y, expected }) => {
     it(`Check ${x} + ${y} == ${expected}`, () => {
-      expect(x + y).to.equal(expected);
+      expect(sumOfTwo(x, y)).to.equal(expected);
+      //      expect(x + y).to.equal(expected);
     });
   });
 });
