1b7c79c9f427d4c37ff122ddc466256ed1d38005
SSN-SPB <smirnov.s68@mail.ru>
2026-09-01 15:56:26 +0200

    js map example enhanced

--- js/js_code_practice/27js_map/27js_map_01basic.js
+++ js/js_code_practice/27js_map/27js_map_01basic.js
@@ -1,5 +1,6 @@
 const testMap = new Map();
 const test_keys = ["a", "b", "c"];
+const test_keys2 = ["a", "c"];
 
 console.log(test_keys);
 console.log(testMap);
@@ -8,3 +9,15 @@ test_keys.forEach((key, val) => testMap.set(key, val + 1));
 
 console.log(test_keys);
 console.log(testMap);
+
+const result = test_keys
+  .filter((key) => testMap.has(key))
+  .map((key) => testMap.get(key) * 3);
+const result2 = test_keys2
+  .filter((key) => testMap.has(key))
+  .map((key) => testMap.get(key) * 3);
+
+console.log(test_keys);
+console.log(testMap);
+console.log(result);
+console.log(result2);
