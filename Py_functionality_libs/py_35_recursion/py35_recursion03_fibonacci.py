f81619267a127148879c0188126d6f11ab62ef9f
SSN-SPB <smirnov.s68@mail.ru>
2026-09-03 15:10:19 +0200

    python recursion fibonacci enhanced with wrapper

--- Py_functionality_libs/py_35_recursion/py35_recursion03_fibonacci.py
+++ Py_functionality_libs/py_35_recursion/py35_recursion03_fibonacci.py
@@ -1,19 +1,38 @@
+# this is a simple fibonacci function with recursion and
+# decorator to check the input argument
+
+
+def checker_fibonacci(func):
+    def wrapper(n):
+        if not isinstance(n, int):
+            return "The argument should be integer"
+        if n < 0:
+            return "The argument should be positive and > 1"
+        else:
+            return func(n)
+
+    return wrapper
+
+
+@checker_fibonacci
 def fibonacci(y):
     if y <= 1:
         return y
     else:
         interim = fibonacci(y - 1) + fibonacci(y - 2)
-        # print(f"Count for {y} interim result is {interim}")
         return interim
 
 
-def main():
+def main(y):
     fibonacci_list = list()
-    for x in range(10):
+    print(f"Start fibonacci list calculating for {y} elements")
+    for x in range(y):
         fibonacci_list.append(fibonacci(x))
 
     print(fibonacci_list)
+    print(fibonacci(-y))
+    print(fibonacci(5.0))
 
 
 if __name__ == "__main__":
-    main()
+    main(10)
