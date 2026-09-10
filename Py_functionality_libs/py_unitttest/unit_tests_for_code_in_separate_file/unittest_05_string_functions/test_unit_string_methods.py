a1f5621efadedf1108efab333ece79dbb1f4bc5a
SSN-SPB <smirnov.s68@mail.ru>
2026-09-10 18:15:05 +0200

    python - basic unittest example - enhanced and improved

--- Py_functionality_libs/py_unitttest/unit_tests_for_code_in_separate_file/unittest_05_string_functions/test_unit_string_methods.py
+++ Py_functionality_libs/py_unitttest/unit_tests_for_code_in_separate_file/unittest_05_string_functions/test_unit_string_methods.py
@@ -1,11 +1,21 @@
 import unittest
 from string_methods import define_string_length
 
+
 class TestStringMethod(unittest.TestCase):
 
     def test_check_length(self):
         result_length = define_string_length("Hello")
         self.assertEqual(result_length, 5)
+
+    def test_check_length_zero_string(self):
+        result_length = define_string_length("")
+        self.assertEqual(result_length, 0)
+
+    def test_check_length_integer(self):
+        with self.assertRaises(TypeError):
+            define_string_length(7)
+
 
 if __name__ == "__main__":
     unittest.main()
