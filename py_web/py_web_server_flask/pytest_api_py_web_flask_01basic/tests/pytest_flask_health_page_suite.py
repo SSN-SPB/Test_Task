e8dc53c74bf57668ae93e573746f19db7a39b51c
SSN-SPB <smirnov.s68@mail.ru>
2026-09-20 11:49:31 +0200

    PYTEST FLASK SERVER - ENHANCED WITH pytest.fixture scope=class

--- py_web/py_web_server_flask/pytest_api_py_web_flask_01basic/tests/pytest_flask_health_page_suite.py
+++ py_web/py_web_server_flask/pytest_api_py_web_flask_01basic/tests/pytest_flask_health_page_suite.py
@@ -1,5 +1,12 @@
+import pytest
+
 from ..resource_dir import resourse_data
 from ..resource_dir.requests import get_page_data
+
+@pytest.fixture(scope="class")
+def health_response(request):
+    endpoint = request.cls.ENDPOINT
+    return get_page_data(endpoint)
 
 
 class TestClassHealthPage:
@@ -8,38 +15,26 @@ class TestClassHealthPage:
     ENV_URL = resourse_data.BASE_URL
     ENDPOINT = ENV_URL + TESTED_PAGE
 
+    def test_code_is_200(self, health_response):
+        response_code, tested_response = health_response
+        checked_value = response_code == 200
+        print("The response code = 200 is: {}".format(checked_value))
+        assert checked_value
 
-# response_code, tested_response = get_page_data(TestClassHealthPage.ENDPOINT)
-
-
-def test_code_is_200():
-    response_code, tested_response = get_page_data(
-        TestClassHealthPage.ENDPOINT
-    )
-    checked_value = response_code == 200
-    print("The response code = 200 is: {}".format(checked_value))
-    assert checked_value
-
-
-def test_status_ok():
-    response_code, tested_response = get_page_data(
-        TestClassHealthPage.ENDPOINT
-    )
-    checked_value = tested_response["status"] == "ok"
-    print(
-        "The key {} has value ok - is: {}".format("status", checked_value)
-    )
-    assert checked_value
-
-
-def test_service_name():
-    response_code, tested_response = get_page_data(
-        TestClassHealthPage.ENDPOINT
-    )
-    checked_value = tested_response["service"] == "flask-training"
-    print(
-        "The key {} has value flask-training - is: {}".format(
-            "service", checked_value
+    def test_status_ok(self, health_response):
+        response_code, tested_response = health_response
+        checked_value = tested_response["status"] == "ok"
+        print(
+            "The key {} has value ok - is: {}".format("status", checked_value)
         )
-    )
-    assert checked_value
+        assert checked_value
+
+    def test_service_name(self, health_response):
+        response_code, tested_response = health_response
+        checked_value = tested_response["service"] == "flask-training"
+        print(
+            "The key {} has value flask-training - is: {}".format(
+                "service", checked_value
+            )
+        )
+        assert checked_value
--- py_web/py_web_server_flask/pytest_api_py_web_flask_01basic/tests/pytest_flask_users_page_suite.py
+++ py_web/py_web_server_flask/pytest_api_py_web_flask_01basic/tests/pytest_flask_users_page_suite.py
@@ -1,5 +1,12 @@
+import pytest
+
 from ..resource_dir import resourse_data
 from ..resource_dir.requests import get_page_data
+
+@pytest.fixture(scope="class")
+def users_response(request):
+    endpoint = request.cls.ENDPOINT
+    return get_page_data(endpoint)
 
 
 class TestClassUsersPage:
@@ -8,32 +15,23 @@ class TestClassUsersPage:
     ENV_URL = resourse_data.BASE_URL
     ENDPOINT = ENV_URL + TESTED_PAGE
 
+    def test_code_is_200(self, users_response):
+        response_code, tested_response = users_response
+        checked_value = response_code == 200
+        print("The response code = 200 is: {}".format(checked_value))
+        assert checked_value
 
-# response_code, tested_response = get_page_data(TestClassUsersPage.ENDPOINT)
-
-
-def test_code_is_200():
-    response_code, tested_response = get_page_data(
-        TestClassUsersPage.ENDPOINT
-    )
-    checked_value = response_code == 200
-    print("The response code = 200 is: {}".format(checked_value))
-    assert checked_value
-
-
-def test_users_are_returned():
-    expected_users = [
-        {"id": 1, "name": "John"},
-        {"id": 2, "name": "Jane"},
-        {"id": 3, "name": "Robert"},
-    ]
-    response_code, tested_response = get_page_data(
-        TestClassUsersPage.ENDPOINT
-    )
-    checked_value = tested_response == expected_users
-    print(
-        "The users response matches the expected data: {}".format(
-            checked_value
+    def test_users_are_returned(self, users_response):
+        expected_users = [
+            {"id": 1, "name": "John"},
+            {"id": 2, "name": "Jane"},
+            {"id": 3, "name": "Robert"},
+        ]
+        response_code, tested_response = users_response
+        checked_value = tested_response == expected_users
+        print(
+            "The users response matches the expected data: {}".format(
+                checked_value
+            )
         )
-    )
-    assert checked_value
+        assert checked_value
